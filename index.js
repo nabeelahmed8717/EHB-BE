fb9ea0ab221a22f29d90305cca6194c5507e1cce
Nabeel Ahmed <nabeel.ahmed.ceative@gmail.com>
2023-11-06 19:33:54 +0500

    setup files / structure

--- index.js
+++ index.js
@@ -13,21 +13,16 @@ const auth = require("./routes/auth");
 const express = require("express");
 const app = express();
 
-if (!config.get("jwtPrivateKey")) {
-  console.log("FATAL ERROR: jwtPrivateKey is not defined.");
-  process.exit(1);
-}
+// if (!config.get("jwtPrivateKey")) {
+//   console.log("FATAL ERROR: jwtPrivateKey is not defined.");
+//   process.exit(1);
+// }
 
-// mongoose
-//   .connect("mongodb://localhost/EHB-BE")
-//   .then(() => console.log("Connected to MongoDB..."))
-//   .catch((err) => console.error("Could not connect to MongoDB..."));
+mongoose
+  .connect("mongodb://localhost/EHB-BE")
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((err) => console.error("Could not connect to MongoDB..."));
 
-const dbUri = config.get("dbUri");
-mongoose.connect(dbUri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
 
 app.use(express.json());
 app.use("/api/genres", genres);
