f97242d31b2caf84a202dff0179cd7053846fb90
Nabeel Ahmed <nabeel.ahmed.ceative@gmail.com>
2023-11-08 23:04:07 +0500

    refactor files

--- index.js
+++ index.js
@@ -2,10 +2,8 @@ const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
 const mongoose = require("mongoose");
+
 const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
 const users = require("./routes/users");
 const auth = require("./routes/auth");
 
@@ -29,14 +27,12 @@ mongoose
 
 
 app.use(express.json());
+
 app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
 app.use("/api/users", users);
 app.use("/api/auth", auth);
 app.use("/", (req, res) => {
-  res.send('Welcome to home page')
+  res.send('Welcome to ehb.com.co')
 })
 
 require('./startup/prod') (app);
