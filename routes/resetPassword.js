299c6e83cdd5705c5cb87553115eb9b326c6f392
nabeel ahmed <nabeel.ahmed.ceative@gmail.com>
2023-11-26 15:14:55 +0500

    #56768 changes in auth logic for encryption

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -38,7 +38,7 @@ router.post("/reset-password-request", async (req, res) => {
         text:
             `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n` +
             `Please click on the following link, or paste this into your browser to complete the process:\n\n` +
-            `${process.env.BASE_URL}/reset-password/${user.resetPasswordToken}\n\n` +
+            `https://ehb-live.com/reset-password/${user.resetPasswordToken}\n\n` +
             `If you did not request this, please ignore this email and your password will remain unchanged.\n`,
     };
 
