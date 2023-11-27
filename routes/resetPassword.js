962a7500aacfad8353e5a71bcd19d8f3bc237e23
nabeel ahmed <nabeel.ahmed.ceative@gmail.com>
2023-11-27 17:29:58 +0500

    #56768 changes in auth logic for encryption

--- routes/resetPassword.js
+++ routes/resetPassword.js
@@ -35,7 +35,7 @@ router.post("/reset-password-request", async (req, res) => {
         from: "ehb.developers@gmail.com",
         to: user.email,
         subject: "Password Reset",
-        text: `
+        html: `
         <div style="display: flex; flex-direction: column; align-items: center;
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;">
         <img src="https://cdn-icons-png.flaticon.com/128/673/673069.png" alt="" style="height: 100px;">
