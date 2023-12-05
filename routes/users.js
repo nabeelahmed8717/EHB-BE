978d14959092ac42e8a7dfcd79a5843e7bb94edd
nabeel ahmed <nabeel.ahmed.ceative@gmail.com>
2023-12-05 18:22:46 +0500

    #56768 check on user name

--- routes/users.js
+++ routes/users.js
@@ -10,8 +10,10 @@ router.post("/", async (req, res) => {
   if (error) return res.status(400).send(error.details[0].message);
 
   let user = await User.findOne({ email: req.body.email });
-
   if (user) return res.status(400).send("User already exists");
+
+  user = await User.findOne({ userName: req.body.userName });
+  if (user) return res.status(400).send("UserName is already taken.");
 
   user = new User(
     _.pick(req.body, [
